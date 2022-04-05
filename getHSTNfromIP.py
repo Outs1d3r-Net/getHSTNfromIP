66bd940bdbe43b47312ffc442926edba2fdffd85
Outs1d3r-Net <41843160+Outs1d3r-Net@users.noreply.github.com>
2022-04-05 16:11:06 -0300

    Create getHSTNfromIP.py

---
+++ getHSTNfromIP.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+
+# ADICIONANDO BIBLIOTECAS.
+import socket    # BIBLIOTECA DE SOCKET PARA CONEXÃO COM IP.
+import sys       # BIBLIOTECA DE SISTEMA PARA INTERAÇÃO COM SISTEMA OPERACIONAL.
+
+
+if len(sys.argv) != 2:
+    print("\n---*======== Get Hostname by IPAddress v1.0 ========*---\n++++++++++++++++++++++++++++++++++++++++++++++++++++\n\nUsage: \n\n", sys.argv[0], "WordlistWithIpAddress.txt\n")
+    sys.exit(1)
+
+
+with open(sys.argv[1], 'r') as listIP:    # LISTANDO IPs DENTRO DA WORDLIST PASSADA PELO PRIMEIRO PARAMETRO.
+    for IP in listIP:
+        try:
+            hostname = socket.gethostbyaddr(IP.strip())    # RESOLVENDO HOSTNAME PELO ENDEREÇO IP.
+            print('IP;'+" ".join(hostname[2])+';Hostname;'+hostname[0])    # SAIDA EM FORMATO PARA ADICIONAR EM PLANILHA
+
+        except (RuntimeError, TypeError, NameError, socket.herror):    # TRATAMENTO DE ERROS
+            pass
